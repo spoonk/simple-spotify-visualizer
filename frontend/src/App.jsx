315a7cbbc6d09d05ed656a25ce676b3cc19529cc
Lucas Cornwell <cornwlucas@gmail.com>
2022-08-21 17:52:58 -0700

    fix for episodes, redo css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,29 +7,32 @@ let scopes = ['user-read-private', 'user-read-email', 'user-read-playback-state'
 let redirectUri = 'http://localhost:3000'
 
 var spotifyApi = new SpotifyWebApi({
-  redirectUri: redirectUri,
-  clientId: clientId
+    redirectUri: redirectUri,
+    clientId: clientId
 });
 
 var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
 
 let params = (new URL(document.location)).searchParams;
-let code = params.get('code'); 
+let code = params.get('code');
 
 function App() {
+    if (!code) {
+        window.location = authorizeURL
+    }
+
+
     const [aCode, setACode] = useState(code);
-  return (
-    <>
-        <div className="">
-            { aCode ?
-                <Dash code={code} />
-                :
-                <div className="container -flex">
-                    <a className="btn btn-success btn-lg" href={authorizeURL}>login</a>
-                </div>
-            }
-        </div>
-    </>
+    return (
+        <>
+            <div className="">
+                {aCode ?
+                    <Dash code={code} />
+                    :
+                    <></>
+                }
+            </div>
+        </>
     );
 }
 
--- frontend/src/Dash.jsx
+++ frontend/src/Dash.jsx
@@ -3,13 +3,13 @@ import useAuth from './useAuth'
 import { useEffect, useState } from "react"
 import SongDisplay from './SongDisplay';
 
-export default function Dash({code, setLoggedIn}) {
+export default function Dash({ code, setLoggedIn }) {
     const [context, setContext] = useState(undefined);
     const accessToken = useAuth(code)
 
     useEffect(() => {
         if (!accessToken) return
-        getSong()        
+        getSong()
     }, [accessToken])
 
     useEffect(() => {
@@ -19,11 +19,11 @@ export default function Dash({code, setLoggedIn}) {
             getSong()
         }, 10000);
         console.log(songInterval)
-        return () => {clearInterval(songInterval)};
+        return () => { clearInterval(songInterval) };
     }, [context]);
 
-    const getSong = async() => {
-        const res = await fetch("https://api.spotify.com/v1/me/player", {
+    const getSong = async () => {
+        const res = await fetch("https://api.spotify.com/v1/me/player?additional_types=track,episode", {
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + accessToken,
@@ -35,9 +35,9 @@ export default function Dash({code, setLoggedIn}) {
         setContext(data)
     }
 
-  return (
-    <div className='dash-container d-flex align-items-center justify-content-center flex-column'>
-        {context && <SongDisplay item={context.item} progress={context.progress_ms} playing={context.is_playing}/>}
-    </div>
-  )
+    return (
+        <div className='dash-container d-flex align-items-center justify-content-center flex-column'>
+            {context && <SongDisplay item={context.item} progress={context.progress_ms} playing={context.is_playing} />}
+        </div>
+    )
 }
